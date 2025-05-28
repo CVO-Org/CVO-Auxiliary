a5a9ede35d7a236c939411b0b3d32f1d4c441bc2
OverlordZorn <dennisAzorn@gmail.com>
2025-05-28 17:05:28 +0200

    kits

---
+++ addons/arsenal/CfgBasePersonalKits.hpp
@@ -0,0 +1,29 @@
+class GVAR(BasePersonalKits) {
+    Base {
+        id = "";
+        condition = "true";
+        addon_dependency = "";
+        class items {};
+        code = ""; // needs to return array
+    };
+
+    class EDITOR_DEBUG: Base {
+        id = "_SP_PLAYER_";
+        class items {
+            class ACE_Banana;
+        };
+        code = "systemChat 'CVO_A_Playerkit test Successful - ACE_Sandbag_empty Given'; 	['ACE_Sandbag_empty']";
+    };
+
+    class Zorn: Base {
+        id = "76561197970306509";
+        class items {
+            class G_Spectacles_Tinted;
+            class G_Balaclava_blk;
+            class H_Beret_blk;
+
+            class B_LegStrapBag_black_F;
+            class ACE_SpraypaintBlack;
+        };
+    };
+};
---
+++ addons/arsenal/CfgBaseRoleKits.hpp
@@ -0,0 +1,86 @@
+class GVAR(BaseRoleKits) {
+    class Base {
+        role = "";
+        condition = "true";
+        addon_dependency = "";
+        class items {};
+        code = "";
+    };
+
+
+    // ACE MEDICAL
+    class BaseRoleKit_Medic: Base {
+        role = "Medic";
+        class items {
+            class ACE_quikclot;
+            class ACE_elasticBandage;
+            
+            class ACE_adenosine;
+
+            class ACE_plasmaIV;
+            class ACE_plasmaIV_500;
+            class ACE_plasmaIV_250;
+
+            class ACE_bloodIV;
+            class ACE_bloodIV_500;
+            class ACE_bloodIV_250;
+
+            class ACE_surgicalKit;
+        };
+    };
+    class BaseRoleKit_Doctor: Base {
+        role = "Doctor";
+        class items {
+        };
+    };
+
+
+    // ACE ENGINEER
+    class BaseRoleKit_Engineer: Base {
+        role = "Engineer";
+        class items {
+            class ACE_wirecutter;
+            class ACE_ToolKit;
+        };
+    };
+    class BaseRoleKit_AdvEngineer: Base {
+        role = "AdvEngineer";
+        class items {
+        };
+    };
+
+
+    // EXPLOSIVE SPECIALIST
+    class BaseRoleKit_ExplosiveSpecialist: Base {
+        role = "ExplosiveSpecialist";
+        class items {
+            class ACE_VMH3;
+            class ACE_VMM3;
+            class ACE_DefusalKit;
+
+            class DemoCharge_Remote_Mag;
+            class SatchelCharge_Remote_Mag;
+        };
+    };
+
+
+    // Machinegunner
+    class BaseRoleKit_Machinegunner: Base {
+        role = "Machinegunner";
+        class items {
+            class ACE_SpareBarrel;
+        };
+    };
+
+
+    // Marksman
+    class BaseRoleKit_Marksman: Base {
+        role = "Marksman";
+        class items {
+            class ACE_SpareBarrel; // Needed?
+
+            class ACE_Tripod;
+            class ACE_RangeCard;
+        };
+    };
+};
