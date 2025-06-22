5ec0b3e7ebef657e6cf5998abffa7621e51c9120
OverlordZorn <dennisAzorn@gmail.com>
2025-06-22 21:32:08 +0200

    Update CfgFunctions.hpp

--- addons/arsenal/CfgFunctions.hpp
+++ addons/arsenal/CfgFunctions.hpp
@@ -12,6 +12,7 @@ class CfgFunctions
 		{
 			file = PATH_TO_FUNC_SUB(internal);
             class postInit { postInit = 1; };
+            class preInit { preInit = 1; };
 
 			class saveStartingLoadout { postInit = 1; };
 
